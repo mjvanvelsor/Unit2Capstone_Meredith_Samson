4fbd3fdd6c88fc87150d8cdd17d456388d9037b1
mjvanvelsor <mjvanvelsor@gmail.com>
2019-08-20 14:24:32 -0400

    empty app files with dependencies

---
+++ admin-api-service/src/main/java/com/trilogyed/adminapiservice/AdminApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.adminapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApiServiceApplication.class, args);
+	}
+
+}
---
+++ levelup-service/src/main/java/com/trilogyed/levelupservice/LevelupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.levelupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelupServiceApplication.class, args);
+	}
+
+}
