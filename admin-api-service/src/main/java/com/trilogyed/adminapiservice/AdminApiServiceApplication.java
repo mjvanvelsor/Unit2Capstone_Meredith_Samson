8b9aa136e2f03c2a6823bd94562b3bce26050f6b
mjvanvelsor <mjvanvelsor@gmail.com>
2019-08-20 15:12:02 -0400

    changes

--- admin-api-service/src/main/java/com/trilogyed/adminapiservice/AdminApiServiceApplication.java
+++ admin-api-service/src/main/java/com/trilogyed/adminapiservice/AdminApiServiceApplication.java
@@ -2,8 +2,16 @@ package com.trilogyed.adminapiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCaching
+// @EnableWebSecurity
 public class AdminApiServiceApplication {
 
 	public static void main(String[] args) {
--- retail-api-service/src/main/java/com/trilogyed/retailapiservice/RetailApiServiceApplication.java
+++ retail-api-service/src/main/java/com/trilogyed/retailapiservice/RetailApiServiceApplication.java
@@ -2,8 +2,16 @@ package com.trilogyed.retailapiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCaching
+@EnableCircuitBreaker
 public class RetailApiServiceApplication {
 
 	public static void main(String[] args) {
